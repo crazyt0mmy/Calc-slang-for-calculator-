b995a6b2dd61dff9ded8b8d152ffc2052285581d
crazyt0mmy <tommaso.baldo008@gmail.com>
2025-02-09 22:53:30 +0100

    Add files via upload

--- Calcolatrice.asm
+++ Calcolatrice.asm
@@ -1,61 +1,45 @@
 .MODEL SMALL
 .STACK 100h
  
-;------------------------------------------------------------------------------------------
-; SEZIONE DATI
-; Qui definiamo tutte le variabili e i messaggi utilizzati nel programma
-;------------------------------------------------------------------------------------------
-
 .DATA 
-   ; Variabili per i numeri e le operazioni
-   VAR1 DW ?            ; Primo numero (16 bit per numeri fino a 65535)
-   VAR2 DW ?            ; Secondo numero (16 bit per numeri fino a 65535)
-   SEGNO1 DB ?          ; Memorizza il segno del primo numero 
-   SEGNO2 DB ?          ; Memorizza il segno del secondo numero
-   OPERATORE DB ?       ; Memorizza l'operatore scelto (+, -, *, /)
-   RIS1 DW ?            ; Risultato dell'operazione prima della conversione
-   RIS2 DW ?            ; Risultato dell'operazione dopo la conversione
+   VAR1 DW ?            
+   VAR2 DW ?            
+   SEGNO1 DB ?          
+   SEGNO2 DB ?          
+   OPERATORE DB ?       
+   RIS1 DW ?            
    
-   ; Messaggi per l'interfaccia utente
    MSG_OPP DB 0Dh, 0Ah, '*Inserire quale operazione si vuole eseguire (+,-,*,/):  $' 
-   MSG1 DB 0Dh, 0Ah, '*Inserire il primo numero (65535): $'
+   MSG1 DB 0Dh, 0Ah, '*Inserire il primo numero: $'
    MSG2 DB 0Dh, 0Ah, '*Inserire il secondo numero: $'
+   MSG3 DB 0Dh, 0Ah, '*Inserire il segno del primo numero (+,-): $'
+   MSG4 DB 0Dh, 0Ah, '*Inserire il segno del secondo numero (+,-): $'
    MSG_RIS DB 0Dh, 0Ah, '*Il risultato corrisponde a: $'
    MSG_ERR1 DB 0Dh, 0Ah, '*Operatore errato, inserirne un operatore valido: $'
    CONTORNO DB '------------------------CALCOLATRICE------------------------$'
-
-;------------------------------------------------------------------------------------------
-; INIZIO DEL CODICE PRINCIPALE
-;------------------------------------------------------------------------------------------
-
+   
 .CODE
 .STARTUP 
-    ; Inizializzazione del segmento dati
     MOV AX, @DATA
     MOV DS, AX 
     
-    ; Visualizza l'interfaccia grafica
     MOV AH, 09H
     LEA DX, CONTORNO
-    INT 21H 
-
-;------------------------------------------------------------------------------------------
-; SEZIONE SELEZIONE OPERATORE
-; Gestisce l'input e la validazione dell'operatore matematico
-;------------------------------------------------------------------------------------------ 
+    INT 21H      
+    
+;-------------------------------------------------------------------------------
+;Richiesta operatore per operazione
+;-------------------------------------------------------------------------------
 
 Richiedi_Operatore:
-    ; Visualizza il messaggio per la scelta dell'operatore
     MOV AH, 09H
     LEA DX, MSG_OPP
     INT 21H 
     
-    ; Legge l'operatore    
     MOV AH, 01H
     INT 21H
     MOV OPERATORE, AL    
     
-    ; Verifica che l'operatore sia valido
     CMP AL, '+'
     JE Leggi_Numeri
      
@@ -68,33 +52,44 @@ Richiedi_Operatore:
     CMP AL, '/'
     JE Leggi_Numeri
     
-    ; Se l'operatore non e' valido, mostra errore e richiedi nuovo input
     MOV AH, 09H
     LEA DX, MSG_ERR1
     INT 21H
     JMP Richiedi_Operatore
-
-;------------------------------------------------------------------------------------------
-; SEZIONE LETTURA NUMERI
-; Gestisce l'input dei due numeri su cui eseguire l'operazione
-;------------------------------------------------------------------------------------------
+    
+;-------------------------------------------------------------------------------
+;Lettura dei numeri e rispettivo segno 
+;-------------------------------------------------------------------------------
 
 Leggi_Numeri:
-    ; Lettura primo numero
+    MOV AH, 09H
+    LEA DX, MSG3
+    INT 21H
+    
+    MOV AH, 01H
+    INT 21H
+    MOV SEGNO1, AL
+
     MOV AH, 09H
     LEA DX, MSG1
     INT 21H
     CALL LeggiNumero
     MOV VAR1, AX
     
-    ; Lettura secondo numero
+    MOV AH, 09H
+    LEA DX, MSG4
+    INT 21H
+    
+    MOV AH, 01H
+    INT 21H
+    MOV SEGNO2, AL     
+    
     MOV AH, 09H
     LEA DX, MSG2
     INT 21H
     CALL LeggiNumero
     MOV VAR2, AX
     
-    ; Determina quale operazione eseguire
     MOV AL, OPERATORE
     
     CMP AL, '+'
@@ -107,142 +102,250 @@ Leggi_Numeri:
     JE Moltiplicazione 
     
     CMP AL, '/'
-    JE Divisione
-
-;------------------------------------------------------------------------------------------
-; SEZIONE OPERAZIONI MATEMATICHE
-; Contiene tutte le operazioni matematiche possibili
-;------------------------------------------------------------------------------------------
+    JE Divisione 
+    
+;-------------------------------------------------------------------------------
+;Addizione
+;-------------------------------------------------------------------------------
 
 Addizione: 
-    MOV AX, VAR1
-    ADD AX, VAR2
-    MOV RIS1, AX
-    JMP Stampa_ris
-        
-Sottrazione:
-    MOV AX, VAR1
-    SUB AX, VAR2
-    MOV RIS1, AX
+    PUSH CX
+    PUSH BX
+    MOV CX, 0 
+    MOV BX, 0
+    
+    MOV BX, VAR1
+    MOV CX, VAR2
+    CMP SEGNO1, '-'
+    JE PrimoNegativo
+    CMP SEGNO2, '-'
+    JE SecondoNegativo
+    
+    ; Caso 1: Entrambi positivi
+    ADD BX, CX 
+    MOV RIS1, BX
+    JMP FineAddizione
+    
+PrimoNegativo:
+    CMP SEGNO2, '-'
+    JE EntrambiNegativi
+    
+    ; Caso 2: Primo negativo, secondo positivo 
+    NEG BX
+    ADD CX, BX
+    MOV RIS1, CX
+    JMP FineAddizione
+    
+SecondoNegativo:
+    ; Caso 3: Primo positivo, secondo negativo 
+    NEG CX
+    ADD BX, CX 
+    MOV RIS1, BX
+    JMP FineAddizione
+    
+EntrambiNegativi:
+    ; Caso 4: Entrambi negativi 
+    NEG BX 
+    NEG CX
+    ADD BX, CX
+    MOV RIS1, BX
+    
+FineAddizione: 
+    POP BX
+    POP CX
+    JMP Stampa_ris  
+    
+;-------------------------------------------------------------------------------
+;Sottrazione
+;------------------------------------------------------------------------------- 
+      
+Sottrazione:  
+    PUSH CX
+    PUSH BX
+    MOV BX, 0
+    MOV CX, 0
+    
+    MOV BX, VAR1 
+    MOV CX, VAR2
+    CMP SEGNO1, '-'
+    JE PrimoNegativo2
+    CMP SEGNO2, '-'
+    JE SecondoNegativo2
+    
+    ; Caso 1: Entrambi positivi
+    SUB BX, CX 
+    MOV RIS1, BX
+    JMP FineSottrazione
+    
+PrimoNegativo2:
+    CMP SEGNO2, '-'
+    JE EntrambiNegativi2
+    
+    ; Caso 2: Primo negativo, secondo positivo 
+    NEG BX
+    SUB CX, BX
+    NEG CX
+    MOV RIS1, CX
+    JMP FineSottrazione
+    
+SecondoNegativo2:
+    ; Caso 3: Primo positivo, secondo negativo 
+    ADD BX, CX 
+    MOV RIS1, BX
+    JMP FineSottrazione
+    
+EntrambiNegativi2:
+    ; Caso 4: Entrambi negativi 
+    NEG BX
+    ADD BX, CX
+    MOV RIS1, BX
+    
+FineSottrazione: 
+    POP BX
+    POP CX
+    JMP Stampa_ris  
+    
+;-------------------------------------------------------------------------------
+;Moltiplicazione
+;------------------------------------------------------------------------------- 
+  
+Moltiplicazione:
+    PUSH BX
+    PUSH CX
+ 
+    MOV BX, 0
+    MOV CX, 0
+    
+    MOV BX, VAR1 
+    MOV CX, VAR2 
+    CMP SEGNO1, '-'
+    JE PrimoNegativo3
+    CMP SEGNO2, '-'
+    JE PrimoNegativo3 
+    
+    MOV AX, BX       
+    MUL CX      
+    MOV RIS1, AX     
+    JMP FineMoltiplicazione
+    
+PrimoNegativo3:
+    CMP SEGNO2, '-'   
+    JE EntrambiNegativi3
+
+    NEG BX            
+    MOV AX, BX        
+    MUL CX       
+    MOV RIS1, AX     
+    JMP FineMoltiplicazione
+
+    
+EntrambiNegativi3:
+    MOV AX, BX        
+    MUL CX        
+    MOV RIS1, AX      
+    
+FineMoltiplicazione:
+    POP BX
+    POP CX
+         
     JMP Stampa_ris
     
-Moltiplicazione: 
-    MOV AX, VAR1
-    MUL VAR2        ; Risultato in DX:AX
-    MOV RIS1, AX     ; Salva solo la parte bassa
-    JMP Stampa_ris
+;-------------------------------------------------------------------------------
+;Divisione
+;-------------------------------------------------------------------------------
     
 Divisione:
-    MOV DX, 0       ; Pulisce DX per la divisione
+    MOV DX, 0       
     MOV AX, VAR1
-    DIV VAR2        ; Quoziente in AX, resto in DX
+    DIV VAR2        
     MOV RIS1, AX
     JMP Stampa_ris
-
-;------------------------------------------------------------------------------------------
-; PROCEDURE DI SUPPORTO
-; Contiene le procedure utilizzate dal programma
-;------------------------------------------------------------------------------------------ 
-
-; Procedura per leggere numeri multi-cifra
-LeggiNumero PROC 
-    PUSH BX         ; Salva i registri
-    PUSH CX
-    
-    MOV BX, 0       ; Inizializza il risultato
-    
-LeggiCifra:
-    MOV AH, 01h     ; Legge un carattere
-    INT 21h
-    
-    CMP AL, 13      ; Se è Invio, termina
-    JE FineLeggi
-    
-    SUB AL, '0'     ; Converte da ASCII a numero
-    MOV CL, AL
-    
-    MOV AX, 10      ; Moltiplica per 10 il numero corrente
-    MUL BX
-    MOV BX, AX
-    
-    ADD BL, CL      ; Aggiunge la nuova cifra
-    
-    JMP LeggiCifra
-
-FineLeggi:
-    MOV AX, BX      ; Prepara il risultato
-    
-    POP CX          ; Ripristina i registri
-    POP BX
-    RET
-LeggiNumero ENDP 
-
-;Procedura per convertire e stampare il risultato con gestione del complemento A2
-Conversione PROC 
-    PUSH BX           ; Salva i registri
-    PUSH CX
-    PUSH DX
-    
-    ; Controlla se il numero è negativo
-    TEST AX, 8000h    
-    JZ PositiveNum    
-    
-    ; Gestione numero negativo
-    PUSH AX           
-    MOV DL, '-'       
-    MOV AH, 02h       
-    INT 21h           
-    POP AX            
-    NEG AX            ; Converti in positivo
-    
-PositiveNum:    
-    XOR CX, CX       ; Inizializza contatore
-    MOV BX, 10       ; Divisore = 10
-    
-DividiNum:
-    XOR DX, DX       ; Pulisci DX prima della divisione
-    DIV BX           ; AX / 10
-    ADD DX, 48       ; Converti in ASCII
-    PUSH DX          ; Salva la cifra nello stack
-    INC CX           ; Incrementa contatore
-    OR AX, AX        ; Verifica se AX è zero
-    JNZ DividiNum    ; Se non è zero, continua
-    
-StampaNum:    
-    POP DX           ; Recupera cifra
-    MOV AH, 02h      ; Funzione stampa carattere
-    INT 21h          ; Stampa
-    LOOP StampaNum   ; Ripeti per tutte le cifre
-    
-    POP DX           ; Ripristina registri
-    POP CX
-    POP BX
-    RET              
-Conversione ENDP
-
-;------------------------------------------------------------------------------------------
-; SEZIONE STAMPA RISULTATO
-; Gestisce la visualizzazione del risultato
-;------------------------------------------------------------------------------------------
 
 Stampa_ris:
     MOV AH, 09H
     LEA DX, MSG_RIS
     INT 21H   
     
-    ;Il risultato viene convertito per stamparlo 
     MOV AX, RIS1
     CALL Conversione 
     
     JMP Fine
-    
-;------------------------------------------------------------------------------------------
-; SEZIONE FINE PROGRAMMA
-; Gestisce la terminazione del programma
-;------------------------------------------------------------------------------------------ 
-
+                  
 Fine:
     MOV AH, 4CH        
-    INT 21h   
+    INT 21h  
+
+;-------------------------------------------------------------------------------
+;Procedure di supporto 
+;-------------------------------------------------------------------------------   
     
-END
+LeggiNumero PROC 
+    PUSH BX         
+    PUSH CX
+    
+    MOV BX, 0       
+    
+LeggiCifra:
+    MOV AH, 01h     
+    INT 21h
+    
+    CMP AL, 13      
+    JE FineLeggi
+    
+    SUB AL, '0'     
+    MOV CL, AL
+    
+    MOV AX, 10      
+    MUL BX
+    MOV BX, AX
+    
+    ADD BL, CL      
+    JMP LeggiCifra
+    
+FineLeggi:
+    MOV AX, BX      
+    
+    POP CX          
+    POP BX
+    RET
+LeggiNumero ENDP
+
+Conversione PROC 
+    PUSH BX           
+    PUSH CX
+    PUSH DX
+    
+    TEST AX, 8000h    
+    JZ NumeroPositivo    
+    
+    PUSH AX           
+    MOV DL, '-'       
+    MOV AH, 02h       
+    INT 21h           
+    POP AX            
+    NEG AX            
+    
+NumeroPositivo:    
+    XOR CX, CX       
+    MOV BX, 10       
+    
+DividiNum:
+    XOR DX, DX       
+    DIV BX           
+    ADD DX, 48       
+    PUSH DX          
+    INC CX           
+    OR AX, AX        
+    JNZ DividiNum    
+    
+StampaNum:    
+    POP DX           
+    MOV AH, 02h      
+    INT 21h          
+    LOOP StampaNum   
+    
+    POP DX           
+    POP CX
+    POP BX
+    RET              
+Conversione ENDP
